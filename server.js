eb958c6adc6b4daae4b4f4cc751f353088468cd5
Osama Jaber <jbr.osama@gmail.com>
2021-05-13 10:36:30 +0300

    basic draft function

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+
+var {replacables} = require('./lettersConfig')
+const {asyncForEach} = require('./utils/asyncForEach')
+
+app.use(express.json())
+
+app.post('/convert', async (req,res)=> {
+    var text = req.body.text;
+    var newText = [];
+    await asyncForEach(text, async(char)=>{
+        console.log(`letter=${char}  code=${escape(char)}  willBeReplacedWith=${replacables[escape(char)]} view=${unescape(replacables[escape(char)])}`)
+        newText.push(replacables[escape(char)] ? unescape(replacables[escape(char)]) : char)
+    });
+    res.send(newText.join(''))
+});
+
+
+// Server
+const port = Number(process.env.PORT || 3000);
+app.listen(port, () => console.log(`App listening on port ${port}!`));
